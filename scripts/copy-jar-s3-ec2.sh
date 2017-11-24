e1a374ab4d286ebbe3f9d76b564e1aaf6336793f
Manish Kumar <manish.kumar@spluspl.com>
2017-11-24 12:38:53 +0530

    commit appspec

---
+++ scripts/copy-jar-s3-ec2.sh
@@ -0,0 +1,1 @@
+aws s3 cp s3://heraj-first-s3-bucket-tesco/target/d98e959f-8f38-4c19-a85c-f5fbbd9230ee/FirstAWS/target/ //mainstorage/zack/Dissertation/Twitter_Stream/Output1hr/ --exclude '*' --include '*2014-01*' --recursive
